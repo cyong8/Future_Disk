3c56e6b947536a14ca26ea417c19c6fe7816dede
Cody Yong <cyong@cs.utexas.edu>
2014-02-13 13:07:55 -0600

    Starting the project...

---
+++ OgreMotionState.h
@@ -0,0 +1,35 @@
+class OgreMotionState:public btMotionState
+{
+protected:
+	Ogre::SceneNode* mVisibelobj;
+	btTransform mPos1;
+public:
+	OgreMotionState(const btTransform& initialpos, Ogre::SceneNode* node)
+	{
+		mVisibleobj = node;
+		mPos1 = initalpos;
+	}
+	virtual ~OgreMotionState(){}
+
+	void setNode(Ogre::SceneNode* node)
+	{
+		mVisibleobj = node;
+	}
+
+	void updateTransform(btTransform& newpos)
+	{
+		mPos1 = newpos;
+	}
+
+	virtual void getWorldTransform(btTransform& worldTrans) const
+	{
+		worldTrans = mPos1;
+	}
+
+	virtual void setWorldTransform(const btTransform& worldTrans)
+	{
+		if(NULL == mVisibleobj)
+			return; // 
+		btQuaternion rot = worldTrans.getRotation();
+	}
+}
