7541364a2fe281c223478b2557497ecf040a2e74
Cody Yong <cyong@cs.utexas.edu>
2014-02-13 13:17:08 -0600

    Updated OgreMotionState from class - not tested for compiling

--- OgreMotionState.h
+++ OgreMotionState.h
@@ -31,5 +31,9 @@ public:
 		if(NULL == mVisibleobj)
 			return; // 
 		btQuaternion rot = worldTrans.getRotation();
+		mVisisbleobj->setOrientation(rot.w(), rot.x(), rot.y(), rot.z());
+		btVector3 pos = worldTrans.getOrigin();
+		mVisibleobj->setPosition(pos.x(), pos.y(), pos.z());
+
 	}
-}
+};
