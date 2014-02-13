c8087d01032d52dfcfec3c0530a36aadc087d13c
Alonso Salas <asalas@cs.utexas.edu>
2014-02-13 14:37:00 -0600

    typed

--- Simulator.h
+++ Simulator.h
@@ -1,15 +1,17 @@
 #ifndef __Simulator_h_
 #define __Simulator_h_
 
+#include "GameObject.h"
+
 class Simulator {
 	protected:
-		btDefaultCollisionConﬁguration* collisionConﬁguration;
+		btDefaultCollissionConfiguration* collisionConfiguration;
 		btCollisionDispatcher* dispatcher;
 		btBroadphaseInterface* overlappingPairCache;
 		btSequentialImpulseConstraintSolver* solver;
 		btDiscreteDynamicsWorld* dynamicsWorld;
 		btConstraintSolver* mConstraintsolver;
-	//  btCollisionWorld* mWorld;
+		//btCollisionWorld* mWorld;
 		Ogre::SceneManager* sceneMgr;
 		std::deque<GameObject*> objList;
 
@@ -18,35 +20,33 @@ class Simulator {
 		~Simulator();
 		void addObject(GameObject* o);
 		bool removeObject(GameObject* o);
-		void stepSimulation(const Ogre::Real elapsedTime,
-				int maxSubSteps = 1, const Ogre::Real ﬁxedTimestep = 1.0f/60.0f);
+		void stepSimulation(const Ogre::Real elapsedTime, 
+				int maxSubSteps = 1, const Ogre::Real fixedTimestep = 1.0f/60.0f);
 };
 
 Simulator::Simulator() {
- 	///collision conﬁguration contains default setup for memory, collision setup.
-	collisionConﬁguration = new btDefaultCollisionConﬁguration();
- 	///use the default collision dispatcher. For parallel processing you can use a diffent dispatcher
-	 dispatcher = new btCollisionDispatcher(collisionConﬁguration);
-	 ///btDbvtBroadphase is a good general purpose broadphase. You can also try out btAxis3Sweep.
+	//collision configuration contains default setup for memory, collision setup.
+	collisionConfiguration = new btDefaultCollisionConfiguration();
+	//use the default collision dispatcher. For parallel processing you can use a different dispatcher
 	overlappingPairCache = new btDbvtBroadphase();
-	 ///the default constraint solver. For parallel processing you can use a different solver (see Extras/BulletMultiThreaded)
-	 solver = new btSequentialImpulseConstraintSolver();
-	dynamicsWorld = new btDiscreteDynamicsWorld(dispatcher,overlappingPairCache,solver,collisionConﬁguration);
+	//the default constraint solver. For parallel processing you can use a different solver (see Extras/BulletMultiThreaded)
+	solver = new btSequentialImpulseConstraintSolver();
+	dynamicsWorld = new btDiscreteDynamicsWorld(dispatcher, overlappingPairCache, solver, collisionConfiguration);
 	dynamicsWorld->setGravity(btVector3(0,-0.098, 0));
-	 //keep track of the shapes, we release memory at exit.
-	 //make sure to re-use collision shapes among rigid bodies whenever possible!
+	//keep track of the shapes, we release memory at exit
+	//make sure to re-use collision shapes among rigit bodies whenever possible!
 	btAlignedObjectArray<btCollisionShape*> collisionShapes;
 }
 
 void Simulator::addObject (GameObject* o) {
 	objList.push_back(o);
-	// use default collision group/mask values (dynamic/kinematic/static)
-	dynamicsWorld->addRigidBody(o->getBody()); 
+	//use default collision group/mask values (dynamic/kinematic/static)
+	dynamicsWorld->addRigidBody(o->getBody());
 }
 
-void Simulator::stepSimulation(const Ogre::Real elapsedTime, int maxSubSteps, const Ogre::Real ﬁxedTimestep) {
-	// do we need to update positions in simulator for dynamic objects?
-	dynamicsWorld->stepSimulation(elapsedTime, maxSubSteps, ﬁxedTimestep);
+void Simulator::stepSimulation(const Ogre::Real elapseTime, int maxSubSteps, const Ogre::Real fixedTimestep){
+	//do we need to update positions in simulator for dynamic objects?
+	dynamicsWorld->stepSimulation(elapseTime, maxSubSteps, fixedTimestep);
 }
 
 #endif // #ifndef __Simulator_h_
