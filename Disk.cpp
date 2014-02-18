2b2b9649ee2a786c7d8e5f60fe00e78b3e5785e3
Kaitlin Decker <kdecker3@cs.utexas.edu>
2014-02-18 14:14:13 -0600

    Added clarifying comments and changed object to scale with worldScale var

--- Disk.cpp
+++ Disk.cpp
@@ -1,14 +1,35 @@
 #include "Disk.h"
 #include "Player.h"
 
-Disk::Disk(Ogre::String nym, Ogre::SceneManager *mgr, Simulator *sim, Ogre::Real height, Ogre::Real radius, Ogre::Vector3 iposition) 
+Disk::Disk(Ogre::String nym, Ogre::SceneManager *mgr, Simulator *sim, Ogre::Real height, Ogre::Real radius, Ogre::Vector3 position) 
 	: GameObject(nym, mgr, sim)
 {
 	heldBy = NULL;
 
+	double scale = 0.1;
+	// Creates a Ogre::Entity for the player using a mesh of our choice
+	Ogre::Entity* ent = mgr->createEntity(nym, "column.mesh");
+
+	// Attach disk to a scene node
+	rootNode->attachObject(ent);
+	// Scale the disk to fit the world - we need the disk in the y-direction to be much smaller
+	rootNode->scale(1.0f/worldScale, 1.0f/(worldScale * 10), 1.0f/worldScale);
+	// Set the position of the disk
+	rootNode->setPosition(position);
+	// Translate the disk - possibly not necesssary? (Kaitlin)
+	rootNode->translate(0,0.1,0);
+
+	//ent->setMaterialName("Examples/Robot");
+
+	// Set collision shape for Bullet
+	//shape = new btCapsuleShape(radius, height);
+	// Set mass of player
+	//mass = 0.1f;
 }
+
 
 void Disk::setPlayer(Player* p)
 {
 	heldBy = p;
 }
+
