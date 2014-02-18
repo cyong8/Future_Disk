842ff1f300355f3ad36e46564d3807a11ddee6de
Kaitlin Decker <kdecker3@cs.utexas.edu>
2014-02-17 20:23:17 -0600

    Changed room to create a bunch of walls and changed wall to use nym as the entity name

--- Room.cpp
+++ Room.cpp
@@ -1,11 +1,30 @@
 #include "Room.h"
 
-Room::Room()
+Room::Room(Ogre::SceneManager *mSceneMgr, Simulator *game_simulator)
 {
-	wallList = vector<Wall>();  // might be unnecessary
+	//wallList = vector<Wall>();  // might be unnecessary
+	//Wall * wallh = (new Wall("Floor", mSceneMgr, game_simulator, Ogre::Vector3(1.0f, 0.01f, 1.0f), Ogre::Vector3(0.0f, 0.0f, 0.0f)));
+	//addWall(wallh);
+	addWalls(wallList,mSceneMgr,game_simulator);
 }
 
-void Room::addWall(Wall w)
+void Room::addWall(Wall *w)
 {
-	((*this).wallList).push_back(w);
+	((*this).wallList).push_back(*w);
+}
+
+void Room::addWalls(vector<Wall> wallList, Ogre::SceneManager *mSceneMgr, Simulator *game_simulator)
+{
+	(new Wall("Floor", mSceneMgr, game_simulator, Ogre::Vector3(1.0f, 0.01f, 1.0f), Ogre::Vector3(0.0f, 0.0f, 0.0f)))->addToSimulator();
+	(new Wall("Ceiling", mSceneMgr, game_simulator, Ogre::Vector3(1.0f, 0.01f, 1.0f), Ogre::Vector3(0.0f, 10.0f, 0.0f)))->addToSimulator();
+	/*
+	(new Wall("wall1", mSceneMgr, game_simulator, Ogre::Vector3(1.0f, 0.01f, 1.0f), Ogre::Vector3(0.0f, 0.0f, 0.0f)))->addToSimulator();
+	(new Wall("wall2", mSceneMgr, game_simulator, Ogre::Vector3(1.0f, 0.01f, 1.0f), Ogre::Vector3(0.0f, 0.0f, 0.0f)))->addToSimulator();
+	(new Wall("wall3", mSceneMgr, game_simulator, Ogre::Vector3(1.0f, 0.01f, 1.0f), Ogre::Vector3(0.0f, 0.0f, 0.0f)))->addToSimulator();
+	(new Wall("wall4", mSceneMgr, game_simulator, Ogre::Vector3(1.0f, 0.01f, 1.0f), Ogre::Vector3(0.0f, 0.0f, 0.0f)))->addToSimulator();
+	(new Wall("wall5", mSceneMgr, game_simulator, Ogre::Vector3(1.0f, 0.01f, 1.0f), Ogre::Vector3(0.0f, 0.0f, 0.0f)))->addToSimulator();
+	(new Wall("wall6", mSceneMgr, game_simulator, Ogre::Vector3(1.0f, 0.01f, 1.0f), Ogre::Vector3(0.0f, 0.0f, 0.0f)))->addToSimulator();
+	(new Wall("wall7", mSceneMgr, game_simulator, Ogre::Vector3(1.0f, 0.01f, 1.0f), Ogre::Vector3(0.0f, 0.0f, 0.0f)))->addToSimulator();
+	(new Wall("wall8", mSceneMgr, game_simulator, Ogre::Vector3(1.0f, 0.01f, 1.0f), Ogre::Vector3(0.0f, 0.0f, 0.0f)))->addToSimulator();
+	*/
 }
--- Wall.cpp
+++ Wall.cpp
@@ -4,11 +4,11 @@ Wall::Wall(Ogre::String nym, Ogre::SceneManager *mgr, Simulator *sim, Ogre::Vect
 	: GameObject(nym, mgr, sim)
 {
 	// 1x1x1 initially set by Ogre
-	Ogre::Entity* ent = mgr->createEntity("FloorEntity", "cube.mesh");
+	Ogre::Entity* ent = mgr->createEntity(nym, "cube.mesh");
 
 	rootNode->attachObject(ent);
 	rootNode->scale(dimensions.x/100, dimensions.y/100, dimensions.z/100);
 	ent->setMaterialName("Examples/Rockwall");
 
-	shape = 
+	//shape = 
 }
