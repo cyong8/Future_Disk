25f987ea7b2f637d558e5ecde47e82681160664d
Alonso Salas <asalas@cs.utexas.edu>
2014-02-17 21:06:08 -0600

    player is a robot

--- Player.cpp
+++ Player.cpp
@@ -1,15 +1,31 @@
 #include "Player.h"
 
-Player::Player(Ogre::String nym, Ogre::SceneManager *mgr, Simulator *sim) 
+Player::Player(Ogre::String nym, Ogre::SceneManager *mgr, Simulator *sim, btScalar height, btScalar radius, Ogre::Vector3 dimensions, Ogre::Vector3 iposition) 
 	: GameObject(nym, mgr, sim)
 {
 	isHolding = false;
 
-	// Need position
+	// Need position, to translate.
 	// Dimensions - so you can make the 
+	Ogre::Entity* ent = mgr->createEntity("PlayerEntity", "robot.mesh");
+
+	rootNode->attachObject(ent);
+	rootNode->scale(0.01, 0.01, 0.01);
+	rootNode->translate(0,0.1,0);
+	ent->setMaterialName("Examples/Robot");
+
+	shape = new btCapsuleShape(radius, height);
+	mass = 0.1f;
 }
 
 void Player::setHolding()
 {
 	isHolding = !isHolding;
+}
+
+void Player::update(float elapsedTime)
+// {
+// 	while(sim->checkHit()){
+		
+// 	}
 }
--- Player.h
+++ Player.h
@@ -6,8 +6,9 @@
 class Player : public GameObject
 {
 public:
-	Player(Ogre::String nym, Ogre::SceneManager *mgr, Simulator *sim);
+	Player(Ogre::String nym, Ogre::SceneManager *mgr, Simulator *sim, Ogre::Real height, Ogre::Real radius, Ogre::Vector3 dimensions, Ogre::Vector3 iposition);
 	void setHolding();
+	void update(float elapsedTime);
 
 protected:
 	bool isHolding;
