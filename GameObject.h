ee88bb6337aaaea120c3c1d51c3dffd72cf9be58
Alonso Salas <asalas@cs.utexas.edu>
2014-02-18 13:02:19 -0600

    testing merge

--- GameObject.h
+++ GameObject.h
@@ -24,6 +24,7 @@ protected:
 	btRigidBody *body;
 	bool kinematic;
 	bool needsUpdates;
+	bool test;
 
 	/* Use this later when we define the struct that contains 
        all the collision info for a given GameObject
