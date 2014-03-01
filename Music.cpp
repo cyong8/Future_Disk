81dca293e9ca15fe290a6d1800cd1cbac56854fb
Cody Yong <cyong@cs.utexas.edu>
2014-02-28 20:41:56 -0600

    Music class finished

--- Music.cpp
+++ Music.cpp
@@ -3,23 +3,74 @@
 
 Music::Music()
 {
-    //SDL_Init(SDL_INIT_AUDIO);
-    
+    SDL_Init(SDL_INIT_AUDIO);
 
-    // if( Mix_OpenAudio( 22050, MIX_DEFAULT_FORMAT, 2, 4096 ) == -1 )
+  	int audio_rate = 22050;
+  	Uint16 audio_format = AUDIO_S16; 
+  	int audio_channels = 2;
+  	int audio_buffers = 4096;
 
-    // music = Mix_LoadMUS( "test.wav");
-    // collisionSound = Mix_LoadWAV("collide.wav");
+    Mix_OpenAudio(audio_rate, audio_format, audio_channels, audio_buffers);
+
+    // Initialise music files
+    pause_StartMusic = Mix_LoadMUS("Pause_StartScreen.wav"); // http://www.freesound.org/people/Erokia/sounds/179287/
+    gameMusic = Mix_LoadMUS("PlayScreen.wav");   // http://www.freesound.org/people/Erokia/sounds/218854/
+    diskToWall = Mix_LoadWAV("BalltoWall.wav");	 // http://www.freesound.org/people/qubodup/sounds/157609/
+    diskToPlayer = Mix_LoadWAV("Catch.wav");     // http://www.freesound.org/people/jorickhoofd/sounds/170623/
+    diskToTarget = Mix_LoadWAV("TargetHit.wav"); // http://www.freesound.org/people/fins/sounds/171671/
+    // MORE MUSIC FOR PLAYER MOVEMENT?
+
+    // Initialise sound channels
+    currentChunkChannel = -1;
+	currentMusicChannel = -1;
 }
 
 Music::~Music()
 {
-
 }
 
 void Music::musicDone()
 {
-	//Mix_FreeChunk(collisionSound);
-    //Mix_FreeMusic(music);
-    //SDL_Quit();
+	Mix_FreeChunk(diskToWall);
+	Mix_FreeChunk(diskToPlayer);
+    Mix_FreeChunk(diskToTarget);
+    Mix_FreeMusic(pause_StartMusic);
+    Mix_FreeMusic(gameMusic);
+
+    SDL_Quit();
+}
+
+void Music::playCollisionSound(string objA, string objB)
+{
+	// Disk To Wall
+	if ((objA == "Disk" && objB == "Player") || (objA == "Player" && objA == "Disk"))
+	{
+		Mix_HaltChannel(currentChunkChannel);
+		currentChunkChannel = Mix_PlayChannel(-1, phaser, 0);
+	}
+	// Disk To Player
+	if (objA == "Disk" && objB == "Wall")
+	{
+		Mix_HaltChannel(currentChunkChannel);
+		currentChunkChannel = Mix_PlayChannel(-1, phaser, 0);
+	}
+	// Disk To Target
+	if (objA == "Disk" && objB == "Target")
+	{
+		Mix_HaltChannel(currentChunkChannel);
+		currentChunkChannel = Mix_PlayChannel(-1, phaser, 0);
+	}
+}
+void Music::playMusic(string state)
+{
+	if (state == "Pause" || state == "Start")
+	{
+		Mix_HaltChannel(currentMusicChannel);
+		currentMusicChannel = Mix_PlayChannel(-1, phaser, -1);
+	}
+	if (state == "Play")
+	{
+		Mix_HaltChannel(currentMusicChannel);
+		currentMusicChannel = Mix_PlayChannel(-1, phaser, -1);
+	}
 }
--- Music.h
+++ Music.h
@@ -1,12 +1,25 @@
 #ifndef __Music_h_
 #define __Music_h_
 
+// SDL headers
+#include "SDL.h"
+#include "SDL_mixer.h"
+#include <stdlib.h>
+
 class Music
 {
-	Mix_Chunk *collisionSound;
-	Mix_Music *music;
+	Mix_Music *pauseMusic;
+	Mix_Music *gameMusic;
+	Mix_Chunk *diskToWall;
+	Mix_Chunk *diskToPlayer;
+	Mix_Chunk *diskToTarget;
+
+	int currentChunkChannel;
+	int currentMusicChannel;
 
 	void musicDone();
+	void playCollisionSound(string objA, string objB);
+	void playMusic(string state);
 };
 
 #endif // #ifndef __Music_h_
